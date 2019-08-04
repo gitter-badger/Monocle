f5e3b5359642b6f0799669f8cddadc4b6a331eb9
David Douglas <david@onetwentyseven.dev>
2019-08-04 01:27:00 -0400

    Oops

--- updater/characters.go
+++ updater/characters.go
@@ -83,7 +83,7 @@ func (u *Updater) updateCharacter(character monocle.Character) {
 		}
 
 		attempts++
-		u.Logger.ErrorF("Would have of thrown error here. Response Code: %d Error: %s", response.Code, err)
+		u.Logger.Errorf(err.Error())
 		time.Sleep(1 * time.Second)
 	}
 
